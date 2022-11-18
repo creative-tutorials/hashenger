9445f84930b54b968817c0828b4d0b6aefeab9a8
Treasure <68476321+Treasure-dell@users.noreply.github.com>
2022-11-18 18:54:27 +0100

    🐛 fixed bug

--- src/ProtectedRoute.tsx
+++ src/ProtectedRoute.tsx
@@ -2,11 +2,11 @@ import { Navigate, Outlet } from "react-router-dom";
 import { useLocation } from "react-router-dom";
 export default function PrivateRoutes () {
   const location = useLocation();
-  const authLogin = localStorage.getItem("chat_session");
+  const authLogin = localStorage.getItem("session.auth");
 
   return authLogin ? (
     <Outlet />
   ) : (
-    <Navigate to="/account" replace state={{ from: location }} />
+    <Navigate to="register" replace state={{ from: location }} />
   );
 };
